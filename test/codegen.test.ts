2727a12d1acb8b28335abe96a60a58cb17754d0c
Doug Binder <douglasjbinder@gmail.com>
2022-03-20 14:33:45 -0500

    chore: more codegen tests

--- test/codegen.test.ts
+++ test/codegen.test.ts
@@ -9,12 +9,19 @@ const files = fs.readdirSync(CONTRACT_DIR);
 
 const assertions = {
   'basic.sol': {},
+  'basic-struct.sol': {},
+  'basic-struct2.sol': {},
+  'basic-foo.sol': {},
+  'basic-mapping.sol': {},
+  'basic-array.sol': {},
+  'basic-enum.sol': {},
+  'basic-erc20.sol': {},
 } as const;
 
 for (let idx in files) {
   let file = files[idx] as keyof typeof assertions;
   if (typeof assertions?.[file] == 'undefined') continue;
-  describe('code gen works', () => {
+  describe(file + ': code gen works', () => {
     it('successfully generates jig contract', async () => {
       const layout = await compileStorageLayout(CONTRACT_DIR + file, 'Basic');
       console.log(generateJig('Basic', layout));
