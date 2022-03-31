232e24d87e7c46e0d3e04b051997eb598bc1b70f
Doug Binder <douglasjbinder@gmail.com>
2022-03-31 10:53:09 -0500

    chore: add foundryDefaults

--- src/utils/foundryConfig.ts
+++ src/utils/foundryConfig.ts
@@ -2,6 +2,12 @@ import * as fs from 'fs';
 import { findNearest } from './findNearest';
 var toml = require('toml');
 
+const foundryConfigDefaults = {
+  default: {
+    src: 'src',
+  },
+};
+
 export function parseToml(fileContent: string) {
   //woop-de-do
   return toml.parse(fileContent);
@@ -10,5 +16,10 @@ export function parseToml(fileContent: string) {
 export function getFoundryConfig() {
   const foundryConfigPath = findNearest('foundry.toml', process.cwd());
   const fileContent = fs.readFileSync(foundryConfigPath, 'utf-8');
-  return parseToml(fileContent);
+  return {
+    default: {
+      ...foundryConfigDefaults.default,
+      ...parseToml(fileContent).default,
+    },
+  };
 }
